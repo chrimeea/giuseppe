30dabd18297c0072399ef3b50cc21aa666aeb8f4
Cristian Mocanu <chrimeea@yahoo.com>
2021-03-06 23:18:58 +0200

    style

--- bin/java.rb
+++ bin/java.rb
@@ -4,16 +4,25 @@
 require_relative 'jvm'
 require 'logger'
 
-def run_main jvm, class_type
-	arrayref = jvm.new_java_array jvm.load_class('[Ljava/lang/String;'), [ARGV.size - 1]
-	ARGV[1..-1].each_with_index { |s, i| arrayref.values[i] = jvm.new_java_string(s) }
-	jvm.run jvm.load_class(class_type), JavaMethod.new('main', '([Ljava/lang/String;)V'), [arrayref]
-rescue JVMError => e
-	method = JavaMethod.new('printStackTrace', '()V')
-	jvm.run e.exception.jvmclass, method, [e.exception]
+class Program
+	def initialize jvm
+		@jvm = jvm
+	end
+
+	def java_array_with_args
+		arrayref = @jvm.new_java_array @jvm.load_class('[Ljava/lang/String;'), [ARGV.size - 1]
+		ARGV[1..-1].each_with_index { |s, i| arrayref.values[i] = @jvm.new_java_string(s) }
+		arrayref
+	end
+
+	def run class_type
+		@jvm.run @jvm.load_class(class_type), JavaMethod.new('main', '([Ljava/lang/String;)V'), [java_array_with_args]
+	rescue JVMError => e
+		@jvm.run e.exception.jvmclass, JavaMethod.new('printStackTrace', '()V'), [e.exception]
+	end
 end
 
 $logger = Logger.new($stdout)
 $logger.level = Logger::ERROR
 $logger.info "Ruby #{RUBY_VERSION} - Running Giuseppe JVM 1.6_1.0 interpreter by Cristian Mocanu"
-run_main JVM.new, ARGV.first
+Program.new(JVM.new).run ARGV.first
